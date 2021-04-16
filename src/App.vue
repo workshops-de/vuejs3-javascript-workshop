11bcb9cfa53c74ea956ffcdd696e1736917212e1
Antony Konstantinidis <ak@digitalessence.de>
2021-04-16 15:02:05 +0200

    Component Lifecycle Basic

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <AppNavigation />
-  <MouseCursor />
-  <InfoBox />
   <BookList />
 </template>
 
 <script>
 import AppNavigation from "./components/AppNavigation/AppNavigation.vue";
-import MouseCursor from "./components/MouseCursor/MouseCursor.vue";
-import InfoBox from "./components/InfoBox/InfoBox.vue";
 import BookList from "./components/BookList/BookList.vue";
 
 export default {
   name: "App",
   components: {
     AppNavigation,
-    MouseCursor,
-    InfoBox,
     BookList,
   },
 };
