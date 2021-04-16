7e5f9496cc146e46b14f1cf8295208ac7124cde1
Antony Konstantinidis <ak@digitalessence.de>
2021-04-16 15:32:55 +0200

    Add BookDetail Route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import BookList from "@/views/BookList/BookList.vue";
 import About from "@/views/About/About.vue";
 import NotFound from "@/views/NotFound/NotFound.vue";
+import BookDetail from "@/views/BookDetail/BookDetail.vue";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/books",
     name: "Books",
     component: BookList,
+  },
+  {
+    path: "/books/:isbn",
+    name: "BookDetail",
+    component: BookDetail,
   },
   {
     path: "/about",
